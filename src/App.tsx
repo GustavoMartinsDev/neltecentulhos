fe0e6f3f71578071f815c7ff1de1542f9d5f9d4d
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2024-02-22 16:58:03 -0300

    Correção rotas url

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import LogoEmpresa from './Assets/logoEmpresa2.jpeg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={LogoEmpresa} className="App-logo" alt="logo" />
-        <h1>
-          Neltec Entulhos
-        </h1>
-        <h4>
-          Em construção.
-        </h4>
+        <h1>Neltec Entulhos</h1>
+        <h4>Em construção.</h4>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/neltecentulhos" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
