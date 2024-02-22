9fc712987c02de6044f1f9f5c935be24a9aa132c
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2024-02-22 02:40:32 -0300

    Publicar site

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import LogoEmpresa from './Assets/logoEmpresa2.jpeg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={LogoEmpresa} className="App-logo" alt="logo" />
+        <h1>
+          Neltec Entulhos
+        </h1>
+        <h4>
+          Em construção.
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
